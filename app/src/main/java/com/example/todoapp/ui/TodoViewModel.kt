20b559f10bdf4ddb1ccee9392f8f74a0cfabe756
carmel26 <nkeshipelagie@gmail.com>
2025-04-17 14:01:52 +0300

    adding todoviewModel on 17April2025 at 14h01

---
+++ app/src/main/java/com/example/todoapp/ui/TodoViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.todoapp.ui
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.todoapp.data.Todo
+import com.example.todoapp.data.TodoDatabase
+import com.example.todoapp.data.TodoRepository
+import kotlinx.coroutines.launch
+
+class TodoViewModel(app: Application) : AndroidViewModel(app) {
+    private val repo: TodoRepository
+    val allTodos: LiveData<List<Todo>>
+        get() {
+             return  allTodos
+        }
+
+    init {
+        val dao = TodoDatabase.getDatabase(app).todoDao()
+        repo = TodoRepository(dao)
+    }
+
+    fun insert(todo: Todo) = viewModelScope.launch { repo.insert(todo) }
+    fun update(todo: Todo) = viewModelScope.launch { repo.update(todo) }
+    fun delete(todo: Todo) = viewModelScope.launch { repo.delete(todo) }
+}
